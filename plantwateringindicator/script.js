552588d1cfd37bc2ba73bcc9c6c67015b4430daf
sripurna samanchi <sripoorna.pura@gmail.com>
2026-03-24 10:55:31 -0700

    Added plant watering app

---
+++ plantwateringindicator/script.js
@@ -0,0 +1,39 @@
+function generateClimate() {
+    const climates = ["Very Sunny", "Moderately Sunny", "Cloudy", "Rainy"];
+    const randomClimate = climates[Math.floor(Math.random() * climates.length)];
+
+    let instruction = "";
+    let bgColor = "";
+    let waterPercent = 0;
+
+    switch(randomClimate) {
+        case "Very Sunny":
+            instruction = "Water plants twice (high water)";
+            bgColor = "#ffcc66";
+            waterPercent = 100;
+            break;
+
+        case "Moderately Sunny":
+            instruction = "Water plants less - twice (medium water)";
+            bgColor = "#ffe699";
+            waterPercent = 70;
+            break;
+
+        case "Cloudy":
+            instruction = "Water plants once (low water)";
+            bgColor = "#cccccc";
+            waterPercent = 40;
+            break;
+
+        case "Rainy":
+            instruction = "Do not water plants";
+            bgColor = "#99ccff";
+            waterPercent = 0;
+            break;
+    }
+
+    document.body.style.backgroundColor = bgColor;
+    document.getElementById("climate").innerText = "Climate: " + randomClimate;
+    document.getElementById("instruction").innerText = instruction;
+    document.getElementById("waterLevel").style.width = waterPercent + "%";
+}
